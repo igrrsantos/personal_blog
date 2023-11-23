e1983261c64764ada54f371ecd5d6afaf34f7c5e
Igor Santos <igrrsantos@gmail.com>
2023-11-22 21:19:55 -0300

    aaaa

--- adjust_simplecov_paths.rb
+++ adjust_simplecov_paths.rb
@@ -1,10 +1,12 @@
+# adjust_simplecov_paths.rb
 require 'json'
 
-file_path = 'coverage/coverage.json' # Caminho do arquivo JSON do SimpleCov
-project_root = '/home/igor/studyspace/manutencao/personal_blog/' # Caminho absoluto que você quer substituir
+coverage_path = 'coverage/.resultset.json'
+coverage_data = JSON.parse(File.read(coverage_path))
 
-if File.exist?(file_path)
-  data = JSON.parse(File.read(file_path))
-  data['coverage'].transform_keys! { |key| key.sub(project_root, '') }
-  File.open(file_path, 'w') { |file| file.write(JSON.pretty_generate(data)) }
+coverage_data["RSpec"]["coverage"].transform_keys! do |key|
+  # Transforma o caminho absoluto em relativo
+  Pathname.new(key).relative_path_from(Pathname.new(Dir.pwd)).to_s
 end
+
+File.write(coverage_path, coverage_data.to_json)
